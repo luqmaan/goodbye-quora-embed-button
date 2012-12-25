944e26da62030bda9749dafd6e5fdddabba27d7e
Luqmaan <intern@PC-3.local>
2012-12-25 16:14:16 -0500

    Overriding the doshow function hid more than just the embed quote button. Now the function only hides the button if the cname is hover_menu

--- goodbye-quora-embed-button/contentscript.js
+++ goodbye-quora-embed-button/contentscript.js
@@ -1,7 +1,15 @@
+// code to inject
 var actualCode = [
-	'window.HoverMenu.prototype.doShow = function () {};'].join('\n');
+	'Function.prototype.clone=function(){var e=this;var t=function(){return e.apply(this,arguments)};t.prototype=e.prototype;for(property in e){if(e.hasOwnProperty(property)&&property!=="prototype"){t[property]=e[property]}}return t};',
+	'var OriginalHoverMenu = window.HoverMenu.prototype.doShow.clone();',
+	'window.HoverMenu.prototype.doShow = function () {',
+	'	if (this.cname !== "hover_menu")',
+	'		OriginalHoverMenu.call(this);',
+	'};'
+].join('\n');
 
+// inject the code
 var script = document.createElement('script');
 script.textContent = actualCode;
-(document.head||document.documentElement).appendChild(script);
+(document.head || document.documentElement).appendChild(script);
 script.parentNode.removeChild(script);
